0d1884d9cc7ebb15d88042a502c05fa448f4da34
Jonathan Dickerson <Jonathan.Dickerson@siu.edu>
2017-11-13 15:27:55 -0600

    removed lab 7 from lab 6

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -1,57 +1,30 @@
 #include <iostream>
 #include <List.h>
+#include <time.h>
+#include <cstdlib>
+
+#define ListSize ( 100 )
 
 using namespace std;
 
-typedef Containters::DLList< int > IntList;
-
-void bubbleSort( IntList* list );
-
 int main()
 {
-	int something;
+	srand( time( NULL ) );
 	typedef Containters::DLList< int > IntList;
 	IntList myList;
 
-	myList.print();
 	int looper = 0;
 
-	for ( ; looper < 100; ++looper )
+	for ( ; looper < ListSize; ++looper )
 	{
-		myList.insert( looper );
+		myList.insert( rand() % 100 );
 	}
-
-	myList.sortList( &bubbleSort );
 
 	myList.print();
+
+	printf("reverse order: \r\n");
+
 	myList.printReverse();
 
-	scanf("%d", something );
 	return 0;
 }
-
-void bubbleSort( IntList* list )
-{
-	IntList::NodeType* first = list->first();
-	IntList::NodeType* last = list->last();
-
-	for ( ; first < last; first = first->next() )
-	{
-		for ( last->previous(); last > first; last = last->previous() )
-		{
-			if ( last->previous()->data() > last->data() )
-			{
-				int temp = last->previous()->data();
-				last->previous()->data() = last->data();
-				last->data() = temp;
-			}
-		}
-	}
-}
-
-//	NodeType* node = BaseType::last();
-//do
-//{
-//	printf("%d, ", node->data() );
-//	node = node->previous();
-//} while ( node != BaseType::last() );
--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -1,9 +1,66 @@
 #include <iostream>
+#include <time.h>
+#include <cstdlib>
+#include "../Lab6/List.h"
+
+#define ListSize ( 100 )
 
 using namespace std;
 
+typedef Containters::DLList< int > IntList;
+
+void bubbleSort( IntList* list );
+
 int main()
 {
-	cout << "Hello World!" << endl;
+	srand( time( NULL ) );
+	typedef Containters::DLList< int > IntList;
+	IntList myList;
+
+	int looper = 0;
+	myList.print();
+
+
+	for ( ; looper < ListSize; ++looper )
+	{
+		myList.insert( rand() % 100 );
+	}
+
+	myList.print();
+
+	myList.sortList( &bubbleSort );
+
+	myList.print();
+
 	return 0;
+}
+
+void bubbleSort( IntList* list )
+{
+	IntList::NodeType* first = list->first();
+	IntList::NodeType* last = list->last();
+	bool didSort = false;
+
+	do
+	{
+		for ( last = list->last(); last > first; last = last->previous() )
+		{
+			if ( last->previous()->data() > last->data() )
+			{
+				didSort = true;
+				int temp = last->previous()->data();
+				last->previous()->data() = last->data();
+				last->data() = temp;
+			}
+		}
+
+		if ( !didSort )
+		{
+			printf("already sorted\r\n");
+			return;
+		}
+
+		first = first->next();
+
+	} while ( first != list->last()->next() );
 }
