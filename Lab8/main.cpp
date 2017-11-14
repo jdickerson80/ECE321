caa0ac59935a838ef7d132ec661ca23a157da9b8
Jonathan Dickerson <Jonathan.Dickerson@siu.edu>
2017-11-14 15:08:13 -0600

    testing change

---
+++ Lab8/main.cpp
@@ -0,0 +1,19 @@
+#include "Tree.h"
+#include "stdio.h"
+using namespace std;
+
+int main()
+{
+//	Containers::Tree< int > tree;
+
+	int loop = 0;
+
+	for ( ; loop < 10; ++loop )
+	{
+		// this is a change
+//		tree.balancedTreeInsert( loop );
+	}
+
+	printf(" depth is %d\r\n", tree.depth() );
+	return 0;
+}
